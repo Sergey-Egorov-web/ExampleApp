33d36970d718a51a11aee7e2d68b0aeb6d4dfe42
Sergey-Egorov-web <ispanezzz.foto@gmail.com>
2024-11-13 06:41:51 +0300

    add post and delete request

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,67 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+//create express app
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 5000;
+const products = [
+    { id: 1, title: "tomato" },
+    { id: 2, title: "orange" },
+];
+const addresses = [
+    { id: 1, value: "Shipilovskaya 45" },
+    { id: 2, value: "flotskaya 41" },
+];
+const parserMiddleware = (0, body_parser_1.default)({});
+app.use(parserMiddleware);
 app.get("/", (req, res) => {
-    let helloMessage = "Hello IT-INCUBATOR!!! I have started!!!!!";
+    let helloMessage = "version 0.01!!!";
     res.send(helloMessage);
+});
+app.get("/products", (req, res) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString();
+        res.send(products.filter((p) => p.title.indexOf(searchString) > -1));
+    }
+    else {
+        res.send(products);
+    }
+});
+app.post("/products", (req, res) => {
+    const newProduct = {
+        id: +new Date(),
+        title: req.body.title,
+    };
+});
+app.get("/products/:id", (req, res) => {
+    let product = products.find((p) => p.id === +req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else
+        res.send(404);
+});
+app.delete("/products/:id", (req, res) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404);
+});
+app.get("/addresses", (req, res) => {
+    res.send(addresses);
+});
+app.get("/addresses/:id", (req, res) => {
+    let address = addresses.find((p) => p.id === +req.params.id);
+    if (address) {
+        res.send(address);
+    }
+    else
+        res.send(404);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,72 @@
 import express, { Request, Response } from "express";
+
+import bodyParser from "body-parser";
+
+//create express app
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+const products = [
+  { id: 1, title: "tomato" },
+  { id: 2, title: "orange" },
+];
+
+const addresses = [
+  { id: 1, value: "Shipilovskaya 45" },
+  { id: 2, value: "flotskaya 41" },
+];
+
+const parserMiddleware = bodyParser({});
+app.use(parserMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
-  let helloMessage = "Hello IT-INCUBATOR!!! I have started!!!!!";
+  let helloMessage = "version 0.01!!!";
   res.send(helloMessage);
+});
+
+app.get("/products", (req: Request, res: Response) => {
+  if (req.query.title) {
+    let searchString = req.query.title.toString();
+    res.send(products.filter((p) => p.title.indexOf(searchString) > -1));
+  } else {
+    res.send(products);
+  }
+});
+app.post("/products", (req: Request, res: Response) => {
+  const newProduct = {
+    id: +new Date(),
+    title: req.body.title,
+  };
+});
+
+app.get("/products/:id", (req: Request, res: Response) => {
+  let product = products.find((p) => p.id === +req.params.id);
+  if (product) {
+    res.send(product);
+  } else res.send(404);
+});
+
+app.delete("/products/:id", (req: Request, res: Response) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === +req.params.id) {
+      products.splice(i, 1);
+      res.send(204);
+      return;
+    }
+  }
+  res.send(404);
+});
+
+app.get("/addresses", (req: Request, res: Response) => {
+  res.send(addresses);
+});
+
+app.get("/addresses/:id", (req: Request, res: Response) => {
+  let address = addresses.find((p) => p.id === +req.params.id);
+  if (address) {
+    res.send(address);
+  } else res.send(404);
 });
 
 app.listen(port, () => {
